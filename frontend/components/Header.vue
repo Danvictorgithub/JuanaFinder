e611e5e268cf68eb126db0879d1df861a31f57c5
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2024-03-17 18:19:12 +0800

    Added Mobile Nav

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -3,16 +3,25 @@
 <template>
   <header class="p-4 border-b-2 border-b-[#111419] border-opacity-50">
     <div class="container mx-auto flex justify-between items-center">
+      <!-- Mobile -->
+      <!-- <button class="block md:hidden">
+        <span class="material-symbols-outlined"> menu </span>
+      </button> -->
+      <!-- Mobile -->
       <NuxtLink to="/" class="flex gap-3 items-center">
         <img src="/logo.svg" class="h-12 w-12" alt="" />
         <h1 class="text-[#00ADB5] text-2xl font-bold">JuanaFinder</h1>
       </NuxtLink>
-      <!-- <div class="flex space-x-4">
-                <NuxtLink class="text-white bg-blue-500 px-4 py-2 rounded">Messages</NuxtLink>
-                <NuxtLink class="text-white bg-blue-500 px-4 py-2 rounded">Notifications</NuxtLink>
-                <NuxtLink class="text-white bg-blue-500 px-4 py-2 rounded">Contracts</NuxtLink>
-            </div> -->
-      <nav class="flex justify-between items-center gap-8">
+      <!-- Mobile -->
+      <nav class="block md:hidden">
+        <NuxtLink
+          class="border-2 border-[#00ADB5] py-2 px-3.5 rounded-md text-[#00ADB5] font-semibold text-lg"
+        >
+          Join
+        </NuxtLink>
+      </nav>
+      <!-- Mobile -->
+      <nav class="hidden md:flex justify-between items-center gap-8">
         <NuxtLink class="text-lg font-semibold text-[#2F414F]">
           Become a Talent
         </NuxtLink>
