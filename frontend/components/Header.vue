aec11d8390805f349090da5269ac95579b215f35
Jhon Dave Bohol <86525008+Davelkling@users.noreply.github.com>
2024-03-17 15:35:57 +0800

    Dashboard

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -5,21 +5,21 @@
 <template>
     <header class=" p-4 border-b-2 border-b-[#111419] border-opacity-50">
         <div class="container mx-auto px-16 flex justify-between items-center">
-            <h1 class="text-[#00ADB5] text-2xl font-bold">JuanaFinder</h1>
+            <h1 class="text-[#00ADB5] text-3xl font-bold">JuanaFinder</h1>
             <!-- <div class="flex space-x-4">
                 <button class="text-white bg-blue-500 px-4 py-2 rounded">Messages</button>
                 <button class="text-white bg-blue-500 px-4 py-2 rounded">Notifications</button>
                 <button class="text-white bg-blue-500 px-4 py-2 rounded">Contracts</button>
             </div> -->
             <div class="flex justify-between items-center gap-8">
-                <button class="text-lg font-semibold text-[#2F414F]">Become a Talent</button>
-                <button class="text-lg font-semibold text-[#2F414F]">Sign In</button>
-                <button class="border-2 border-[#00ADB5] py-0.5 px-3.5 rounded-md text-[#00ADB5] font-semibold text-lg">Join</button>
+                <button class="text-lg font-semibold text-[#2F414F] hover:bg-gray-100 rounded-3xl p-2 px-4">Become a Talent</button>
+                <button class="text-lg font-semibold text-[#2F414F] hover:bg-gray-100 rounded-3xl p-2 px-4">Log In</button>
+                <button class="border-2 border-[#00ADB5] hover:bg-[#00ADB5] hover:text-[#F1F1EF] py-0.5 px-3.5 rounded-md text-[#00ADB5] font-semibold text-lg">Sign Up </button>
             </div>
         </div>
     </header>
 </template>
 
 <style scoped>
-    /* Add any additional styles here */
+
 </style>
